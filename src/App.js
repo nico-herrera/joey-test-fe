c700cf6acebfb0f16a84359331824a2625325c49
Nico Herrera <nicoherrera0831@gmail.com>
2021-07-22 20:47:23 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [input, setInput] = useState({
+    name: "",
+    value: 0,
+  })
+  const [data, setData] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    try {
+      setIsLoading(true)
+      console.log(input)
+      let res = await axios.post("http://localhost:5000/api/test", {
+        name: input.name,
+        value: Number(input.value)
+      })
+      setIsLoading(false)
+      setData(res.data)
+    } catch (err) {
+      console.log(err)
+  }
+  }
+  const changeHandler = (e) => {
+    setInput({...input, [e.target.name]: e.target.value})
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" type="text" placeholder="Name" value={input.name} onChange={changeHandler} />
+        <label htmlFor="value">Value</label>
+        <input id="value" name="value" type="number" placeholder="value" value={input.value} onChange={changeHandler} />
+        <button>Submit</button>
+      </form>
+
+      <div>Name total = {data}</div>
     </div>
   );
 }
